eafed370140f776b2d0da91b9c8590153c24509b
lhuan <lhuanmarcos@gmai.com>
2026-01-13 18:57:28 -0300

    Inclusão cenário 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,16 +5,28 @@ class Program
 {
     static void Main()
     {
+        Cenario1();
+        Cenario2();
+
+        Console.ReadKey();
+    }
+    static void Cenario1()
+    {
         int[] arrayEntrada = { 3, 2, 1, 6, 0, 5 };
 
         var arvore = ConstrutorArvore.Construir(arrayEntrada);
 
         Console.WriteLine("Árvore gerada:\n");
         ImprimirArvore(arvore, 0);
-
-        Console.ReadKey();
     }
+    static void Cenario2()
+    {
+        int[] arrayCenario2 = { 7, 5, 13, 9, 1, 6, 4 };
+        var arvore = ConstrutorArvore.Construir(arrayCenario2);
 
+        Console.WriteLine("Árvore gerada (Cenário 2):\n");
+        ImprimirArvore(arvore, 0);
+    }
     static void ImprimirArvore(Arvore no, int nivel)
     {
         if (no == null) return;
